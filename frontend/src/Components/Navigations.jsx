1c4ff4186adcbd40e933effe2a7f339c9ed77e3b
Gopal soni <Gopals.btech2022@iujaipur.edu.in>
2024-07-16 17:57:40 +0530

    little bugs

--- frontend/src/Components/Navigations.jsx
+++ frontend/src/Components/Navigations.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 
 const Navigations = () => {
   return (
-    <div className="flex flex-wrap justify-around gap-4 ">
-    <div className="w-1/3 h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
+    <div className="grid grid-cols-1 gap-2 md:grid md:grid-cols-2  lg:grid lg:grid-cols-3 ">
+    <div className="w-[400px] h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
       <img className='h-[350px]'src="https://img.freepik.com/free-photo/collage-city-committed-education_23-2149886993.jpg?w=1060&t=st=1721131435~exp=1721132035~hmac=7e9511f9cf3007bc0fa9ef50aa508c2b2c3184a40829617c666570081a8dd01c" />
       <h2 className="text-center text-lg font-bold mt-2">Admission</h2>
     </div>
-    <div className="w-1/3 h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
+    <div className="w-[400px] h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
       <img className='h-[350px]' src="https://img.freepik.com/premium-photo/businessman-using-mobile-smart-phone-searching-information-location-point_34683-2739.jpg?w=1060" />
       <h2 className="text-center text-lg font-bold mt-2">Contact</h2>
     </div>
-    <div className="w-1/3 h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
+    <div className="w-[400px] h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
       <img className='h-[350px]' src="https://imgs.search.brave.com/HEOY8CtcH1a8fhqzvaY_zYFcifzZDKh3K6_hdbtQkW4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAwLzU0LzIxLzg2/LzM2MF9GXzU0MjE4/NjI5X3BjRzhxT0Rh/R1J2UVF2TjFXMEVR/MUxCbzhOYUdNSUEx/LmpwZw" alt="School Building" className="w-full rounded-md" />
       <h2 className="text-center text-lg font-bold mt-2">About us</h2>
     </div>
-    <div className="w-1/3 h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
+    <div className="w-[400px] h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
       <img className='h-[350px]' src="https://img.freepik.com/free-photo/closeup-hands-passing-contract-unrecognizable-businessman_1098-19612.jpg?t=st=1721131101~exp=1721134701~hmac=0020d42ab53e2fae3b2ae8c26202c06ac2cd59d8cf82cdcbb265467c373d8c8d&w=1060" alt="Disclosure" className="w-full rounded-md" />
       <h2 className="text-center text-lg font-bold mt-2">Disclosure</h2>
     </div>
-    <div className="w-1/3 h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
+    <div className="w-[400px] h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
       <img className='h-[350px]' src="https://img.freepik.com/free-photo/group-people-smiling-happy-having-paint-class-art-studio_839833-9776.jpg?t=st=1721131046~exp=1721134646~hmac=986ae7a87e5b1b69d559bb76b191ca875a648d5bca65d4fa9c37d5a46dc280be&w=1060" alt="Gallery" className="w-full rounded-md" />
       <h2 className="text-center text-lg font-bold mt-2">Gallery</h2>
     </div>
-    <div className="w-1/3 h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
+    <div className="w-[400px] h-[400px] bg-gray-200 p-4 rounded-md shadow-md">
       <img className='h-[350px]' src="https://img.freepik.com/premium-photo/rear-view-road-sign-railing-city_1048944-20227752.jpg?w=1060" alt="Certificates" className="w-full rounded-md" />
       <h2 className="text-center text-lg font-bold mt-2">TC</h2>
     </div>
